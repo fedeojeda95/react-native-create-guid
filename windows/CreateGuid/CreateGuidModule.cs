3d6b38bad41746a17259342917079ca464280423
Andrew Stevens <aks427@gmail.com>
2021-02-17 16:54:32 -0800

    initial work on windows support

---
+++ windows/CreateGuid/CreateGuidModule.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ReactNative.Managed;
+using System;
+
+namespace CreateGuid
+{
+    [ReactModule("CreateGuid")]
+    public sealed class CreateGuidModule
+    {
+        [ReactMethod("createGuid")]
+        public string CreateGuid()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
